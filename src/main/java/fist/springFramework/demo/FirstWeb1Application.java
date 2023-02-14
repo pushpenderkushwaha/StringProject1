dd8f63b86aaae4e7b2ece20d97c76c0f4b26c9da
Pushpender Kushwaha <pushpenderk@microwize.com>
2023-02-14 12:19:35 +0530

    initial string framework project

---
+++ src/main/java/fist/springFramework/demo/FirstWeb1Application.java
@@ -0,0 +1,13 @@
+package fist.springFramework.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWeb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/fist/springFramework/demo/FirstWeb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fist.springFramework.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWeb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
